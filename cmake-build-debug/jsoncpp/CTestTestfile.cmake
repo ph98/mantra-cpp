356f7f32be063713f27d53ebfc6c722b4902ad80
parham heidari <heidari1377@gmail.com>
2019-02-09 15:10:09 +0330

    ignore cmake

--- cmake-build-debug/jsoncpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mohamad/game/mantra-cpp/jsoncpp
-# Build directory: /home/mohamad/game/mantra-cpp/cmake-build-debug/jsoncpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("include")
