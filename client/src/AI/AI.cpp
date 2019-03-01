4fd4dd044b2001e52272b1951f31895a8def27ef
parham heidari <heidari1377@gmail.com>
2019-03-01 16:30:26 +0330

    doing first attack

--- client/src/AI/AI.cpp
+++ client/src/AI/AI.cpp
@@ -20,7 +20,6 @@ constantClass constants1 ;
 void AI::preProcess(World *world) {
     // cout<< "Turn -> " << world->getCurrentTurn()<<endl ; 
     fill_dist_to_objective(world , my_map) ;
-    // fill_map_weights(world);
 }   
 
 void AI::pick(World *world) {
@@ -28,7 +27,7 @@ void AI::pick(World *world) {
     constants1.pick_phase++ ;
 }
 void AI::move(World *world) {
-    print_map(world);
+    // print_map(world);
     for(auto hero : world->getMyHeroes() ){
         int heroRow = hero->getCurrentCell().getRow() ;
         int heroCol = hero->getCurrentCell().getColumn() ; 
@@ -41,5 +40,45 @@ void AI::move(World *world) {
 
 void AI::action(World *world) {
     // print_map(world);
+
+    fill_map_weights(world ,  my_map);
+
+    // for(int i = 0; i <32; i++)
+    // {
+    //     for(int j = 0; j <32 ; j++)
+    //     {
+    //         cout<< my_map[i][j].weight[BLASTER_BOMB] << setw(4) ; 
+    //     }
+    //     cout<<"\n";
+    // }
+    int maxScore = 0 ; 
+    for(auto cell : world->getMap().getObjectiveZone()){
+        if(maxScore <= my_map[cell->getRow()][cell->getColumn()].weight[BLASTER_BOMB] ){
+            maxScore = my_map[cell->getRow()][cell->getColumn()].weight[BLASTER_BOMB] ;
+            constants1.distination_row = cell->getRow() ;
+            constants1.distination_col = cell->getColumn() ;
+        }
+    }
+
+    for(auto hero : world->getMyHeroes()){
+        int target_col =0 ; 
+        int target_row =0 ; 
+        int max_target = 0 ; 
+        int Range = world->getAbilityConstants()[BLASTER_BOMB]->getRange() ;
+        int i = hero->getCurrentCell().getRow() ;
+        int j = hero->getCurrentCell().getColumn() ;
+
+        for(int i1 = -Range; i1 <= +Range; i1++)
+            for(int j1 = -Range; j1 <= +Range; j1++)
+            {
+                if( world->getMap().isInMap(i + i1 , j+j1) && world->manhattanDistance(i , j , i1 , j1 ) <= Range )
+                    if(max_target <= my_map[i + i1][j+ j1].weight[BLASTER_BOMB] ){
+                        max_target = my_map[i + i1][j+ j1].weight[BLASTER_BOMB] ;
+                        target_row = i + i1 ;
+                        target_col = j+ j1 ;
+                    }
+            }
+        world->castAbility(hero->getId() , BLASTER_BOMB , target_row , target_col);
+    }
 
 }
--- client/src/AI/fill_the_map/fill_the_map.cpp
+++ client/src/AI/fill_the_map/fill_the_map.cpp
@@ -23,34 +23,29 @@ void fill_dist_to_objective(World *world  , mycell my_map[][MAX_HEIGHT] )
 
 
 void fill_map_weights(World *world   , mycell my_map[][MAX_HEIGHT]){
-
-    int ans = 0 ; 
       // make all zero each turn :
-    cout<< world->getMap().getColumnNum() <<endl ;
     for(int i = 0; i < world->getMap().getColumnNum(); i++)
         for(int j = 0; j < world->getMap().getRowNum() ; j++)
             for(auto ability : world->getAbilityConstants())
                 my_map[i][j].weight[ability->getName()] = 0;
 
     // fill the weights : 
-    for(int i = 0; i < world->getMap().getColumnNum(); i++)
+    for(int i = 0; i < world->getMap().getColumnNum(); i++){
         for(int j = 0; j < world->getMap().getRowNum() ; j++){
-            for(auto ability : world->getAbilityConstants() ){
-                int Range = ability->getRange() ;
+            // for(auto ability : world->getAbilityConstants() ){
+                int Range = world->getAbilityConstants()[BLASTER_BOMB]->getRange() ;
                 for(int i1 = -Range; i1 <= +Range; i1++)
                     for(int j1 = -Range; j1 <= +Range; j1++)
                     {
                         if( world->getMap().isInMap(i + i1 , j+j1) && world->manhattanDistance(i , j , i1 , j1 ) <= Range )
                             if( world->getOppHero(i + i1 , j + j1 ).getCurrentHP() > 0 ){
-                                my_map[i][j].weight[ability->getName() ] += ( constants.max_attack_weight - world->getOppHero(i + i1 , j + j1 ).getCurrentHP()  );
-                                 cout<< "--> " << my_map[i][j].weight[ability->getName() ] <<endl ;
+                                cout<<"hp" << world->getOppHero(i + i1 , j + j1 ).getCurrentHP()<<endl; 
+                                my_map[i][j].weight[BLASTER_BOMB] += ( constants.max_attack_weight - world->getOppHero(i + i1 , j + j1 ).getCurrentHP()  );
                             }
-                            //  cout<< "--> " << world->getOppHero(i + i1 , j + j1 ).getCurrentHP()<<endl ;
                     }
-            }
+            // }
+
         }
-
-
-    cout<<ans<<endl ; 
-
+    }
+    // cout<<"1" ;
 }
